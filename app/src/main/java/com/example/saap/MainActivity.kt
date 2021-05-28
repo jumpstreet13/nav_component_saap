314f575cb3097c79d941f729826a882085ed8ab2
Магомедов Абакар Абдулнасирович <AMagomedov2@alfabank.ru>
2021-05-28 18:16:19 +0300

    remove comments

--- app/src/main/java/com/example/saap/MainActivity.kt
+++ app/src/main/java/com/example/saap/MainActivity.kt
@@ -17,13 +17,16 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home,
-            R.id.navigation_dashboard, R.id.navigation_notifications))
+            R.id.navigation_dashboard,
+            R.id.navigation_notifications)
+        )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
+
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (
                 destination.id == R.id.navigation_heroes ||
